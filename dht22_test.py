e766d83c14ddb0e63abb2d458db7dbf88d8d0020
rps57 <73319113+rps57@users.noreply.github.com>
2021-02-11 17:29:08 -0600

    Update dht22_test.py

--- dht22_test.py
+++ dht22_test.py
@@ -8,17 +8,12 @@ import time
 import board
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
+# initialize dht device 
 dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
-
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
     try:
-        # Print the values to the serial port
+        # print values to serial port 
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
@@ -29,7 +24,6 @@ while True:
         )
 
     except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
         time.sleep(2.0)
         continue
@@ -37,6 +31,7 @@ while True:
         dhtDevice.exit()
         raise error
 
+	# time between samples
     time.sleep(3.0)
 
 
