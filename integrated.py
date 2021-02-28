e07a563e4c4f97a9669007fa6e1253eca49d0a47
rps57 <73319113+rps57@users.noreply.github.com>
2021-02-28 16:27:12 -0600

    Update integrated.py

--- integrated.py
+++ integrated.py
@@ -13,64 +13,39 @@ import adafruit_dht
 import adafruit_gps
 
 
-################### DHT22 temp/humidity collection ###################
+################### DHT22 temp/humidity collection and logging ###################
 
-# initialize dht device 
 dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-while True:
+f = open('/home/pi/Project/dht22_sal.csv', 'a+') # this file may need to change
+if os.stat('/home/pi/Project/dht22_sal.csv').st_size == 0:
+    f.write('Date,Time,Temperature,Humidity\r\n')
+running = True
+while running:
+
     try:
-        # print values to serial port 
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
+
+        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+
+        if humidity is not None and temperature_c is not None:
+            f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature_c, humidity))
+            time.sleep(3)
+
+
+        else:
+            print("failed to read sensor")
 
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(2.0)
         continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-	# time between samples
-    time.sleep(3.0)
-	
-	
-
-################### Temperature and humidity datalogging ###################
-
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
-
-# open pre-made file for data logs
-try:
-    f = open('/home/pi/dht22_logging.csv', 'a+') # this file may need to change
-    if os.stat('/home/pi/dht22_logging.csv').st_size == 0:
-            f.write('Date,Time,Temperature,Humidity\r\n')
-# do nothing			
-except:
-    pass
-
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-
-	# if data is collected, store it
-    if humidity is not None and temperature is not None:
-       f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity)) 
-    else:
-        print("Failed to retrieve data from humidity sensor")
-
-    time.sleep(20)
-
-
-
+    except KeyboardInterrupt:
+        print("Program stopped")
+        running = False
+        f.close()
 
 ################### GPS location collection ###################
 
