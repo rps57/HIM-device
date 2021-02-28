d1f9acdae211b28a1205276de2d9ef99e2dd6192
rps57 <73319113+rps57@users.noreply.github.com>
2021-02-28 13:57:46 -0600

    Update integrated1.py

--- integrated1.py
+++ integrated1.py
@@ -45,29 +45,29 @@ while True:
 
 ################### Temperature and humidity datalogging ###################
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+#DHT_SENSOR = Adafruit_DHT.DHT22
+#DHT_PIN = 4
 
 # open pre-made file for data logs
-try:
-    f = open('/home/pi/HIM-device/dht22_logging.csv', 'a+') # this file may need to change
-    if os.stat('/home/pi/HIM-device/dht22_logging.csv').st_size == 0:
-            f.write('Date,Time,Temperature,Humidity\r\n')
+#try:
+#    f = open('/home/pi/HIM-device/dht22_logging.csv', 'a+') # this file may need to change
+#    if os.stat('/home/pi/HIM-device/dht22_logging.csv').st_size == 0:
+#            f.write('Date,Time,Temperature,Humidity\r\n')
 # do nothing			
-except:
-    pass
+#except:
+#    pass
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+#while True:
+#    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
 
     # if data is collected, store it
-    if humidity is not None and temperature is not None:
-       f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity)) 
-    else:
-        print("Failed to retrieve data from humidity sensor")
+#    if humidity is not None and temperature is not None:
+#       f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity)) 
+#    else:
+#        print("Failed to retrieve data from humidity sensor")
 
-    time.sleep(20)
+#    time.sleep(20)
 
 
 
